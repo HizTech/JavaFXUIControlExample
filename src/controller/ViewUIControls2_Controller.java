1e624eee01c8c39433ae4f14a6285a259c4e3981
georgesk24 <jorgepotes1994@gmail.com>
2021-02-23 15:59:58 -0500

    update combobox
    update

--- src/controller/ViewUIControls2_Controller.java
+++ src/controller/ViewUIControls2_Controller.java
@@ -1,12 +1,19 @@
 package controller;
 
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.ResourceBundle;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.CheckBox;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.RadioButton;
 import javafx.scene.control.Toggle;
 import javafx.scene.control.ToggleGroup;
@@ -32,7 +39,9 @@ public class ViewUIControls2_Controller implements Initializable {
     @FXML
     private ImageView containerImage;
     
-    
+    @FXML
+    private ComboBox<String> combobox1, combobox2;
+        
     @FXML
     private void mouseEvent(MouseEvent event){
     
@@ -46,6 +55,20 @@ public class ViewUIControls2_Controller implements Initializable {
         
     
     } 
+        
+    @FXML
+    private void comboboxEvent(ActionEvent e){
+    
+        Object evt = e.getSource();
+        
+        if(evt.equals(combobox1)){
+            
+            System.out.println(combobox1.getSelectionModel().getSelectedItem());
+        
+        }
+    
+    }
+    
     
     
     @Override
@@ -55,21 +78,26 @@ public class ViewUIControls2_Controller implements Initializable {
         radioCalendar.setUserData("calendar");
         radioContacts.setUserData("contact");
         
-        group.selectedToggleProperty().addListener(new ChangeListener<Toggle>() {
-            @Override
-            public void changed(ObservableValue<? extends Toggle> observable, Toggle oldValue, Toggle newValue) {
+        group.selectedToggleProperty().addListener((ObservableValue<? extends Toggle> observable, Toggle oldValue, Toggle newValue) -> {
+            if(group.getSelectedToggle() != null){
                 
-                if(group.getSelectedToggle() != null){
-                    
-                    Image image = new Image(
-                    getClass().getResourceAsStream("/images/"+group.getSelectedToggle().getUserData().toString()+".png"));
+                Image image = new Image(
+                        getClass().getResourceAsStream("/images/"+group.getSelectedToggle().getUserData().toString()+".png"));
                 
-                    containerImage.setImage(image);
-                    
-                }
-
+                containerImage.setImage(image);
+                
             }
         });
+                
+        ArrayList<String> list = new ArrayList<>();
+        Collections.addAll(list, new String[]{"Masculino", "Femenino"});
+        
+        combobox1.getItems().addAll(list);
+                
+        combobox2.getItems().add("Maria@gmail.com");
+        combobox2.getItems().add("jose@gmail.com");
+        combobox2.getItems().add("lusia@gmail.com");        
+        
         
         
     }    
